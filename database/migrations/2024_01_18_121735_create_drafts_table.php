e7e397b81fac07626505cffdd92810aee7a30668
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2024-01-18 12:18:37 +0000

    Migration

---
+++ database/migrations/2024_01_18_121735_create_drafts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drafts', function (Blueprint $table) {
+            $table->id();
+            $table->string('draftable_type')->index();
+            $table->integer('draftable_id')->index()->nullable();
+            $table->integer('author_id');
+            $table->text('payload');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drafts');
+    }
+};
